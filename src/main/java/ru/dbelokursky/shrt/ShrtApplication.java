49e080e1097a606c3ab18190dbc23b5bbe002811
Dmitry Belokursky <mailto:dbelokursky@gmail.com>
2018-09-12 23:45:45 +0500

    initial

---
+++ src/main/java/ru/dbelokursky/shrt/ShrtApplication.java
@@ -0,0 +1,12 @@
+package ru.dbelokursky.shrt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShrtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShrtApplication.class, args);
+    }
+}
