d7d7a7f05a4e8e346a0ea4caf49c6a578539b59a
Dmitry Belokursky <dbelokursky@gmail.com>
2018-09-14 14:21:45 +0500

    fix inti.sql

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -23,18 +23,17 @@ name VARCHAR(20) UNIQUE
 CREATE TABLE user_role(
 user_id INTEGER REFERENCES shrt_user(user_id),
 role_id INTEGER REFERENCES role(role_id),
-UNIQUE (owner_id, role_id)
+UNIQUE (user_id, role_id)
 );
-
 
 -- Insert data
 INSERT INTO role (name) VALUES ('ROLE_USER');
 INSERT INTO role (name) VALUES ('ROLE_ADMIN');
 
 -- user1 with password1, user2 with password2 etc.
-INSERT INTO user (login, password, enabled) VALUES ('user1', '$2a$10$eoIqcMQxa9oubfEa9wv.GOTpOaIudEQmCTr4F.jLzkerJuumAyu6C', TRUE);
-INSERT INTO user (login, password, enabled) VALUES ('user2', '$2a$10$DpIj6lvOCFCrd461pfo9r.AP5koi6sN1g2SDJzLHYvHRzGjCFD6A.', TRUE);
-INSERT INTO user (login, password, enabled) VALUES ('user3', '$2a$10$uoyWPgbo/ujJljySz.rUX.5qZBIFkGri9yuZJExWvJfHDZ/45nhnu', TRUE);
+INSERT INTO shrt_user (login, password, enabled) VALUES ('user1', '$2a$10$eoIqcMQxa9oubfEa9wv.GOTpOaIudEQmCTr4F.jLzkerJuumAyu6C', TRUE);
+INSERT INTO shrt_user (login, password, enabled) VALUES ('user2', '$2a$10$DpIj6lvOCFCrd461pfo9r.AP5koi6sN1g2SDJzLHYvHRzGjCFD6A.', TRUE);
+INSERT INTO shrt_user (login, password, enabled) VALUES ('user3', '$2a$10$uoyWPgbo/ujJljySz.rUX.5qZBIFkGri9yuZJExWvJfHDZ/45nhnu', TRUE);
 
 -- owner1 with role USER and owner2 role USER and ADMIN
 INSERT INTO user_role(user_id, role_id) VALUES (1,1);
